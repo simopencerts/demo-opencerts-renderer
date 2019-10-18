69ccd68cf8a57f38cf529ef2984d21b9e967e03e
simopencerts <56102893+simopencerts@users.noreply.github.com>
2019-10-18 15:31:29 +0800

    Create index.js

---
+++ src/components/templates/SIMCert/index.js
@@ -0,0 +1,17 @@
+import SIMCert from "./certificate";
+import SIMTranscript from "./transcript";
+
+const templates = [
+  {
+    id: "certificate",
+    label: "Certificate",
+    template: SIMCert
+  },
+  {
+    id: "transcript",
+    label: "Transcript",
+    template: SIMTranscript
+  }
+];
+
+export default templates;
