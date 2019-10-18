51804cfa44ade68aee93dbfed13ba3c1d12d8540
simopencerts <56102893+simopencerts@users.noreply.github.com>
2019-10-18 15:29:25 +0800

    Create certificate.js

---
+++ src/components/templates/SIMCert/certificate.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { format } from "date-fns";
+import { tz } from "moment-timezone";
+import { get } from "lodash";
+import {
+  renderLogoSIM,
+  renderAwardText,
+  renderTwoSignatures,
+  renderFooter
+} from "./common/certificate";
+
+const Template = ({ certificate }) => (
+  <div>
+    <div className="container" style={{ border: 0, borderColor: "#AAA", borderStyle: "solid" }}>
+      {renderLogoSIM()}
+      {renderAwardText(certificate)}
+      {renderTwoSignatures(certificate)}
+    </div>
+	
+    {renderFooter(certificate)}
+  </div>
+);
+
+Template.propTypes = {
+  certificate: PropTypes.object.isRequired
+};
+export default Template;
